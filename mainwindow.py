9b06ce1b9fe21b3cab3699670dba73390c2ecb99
00urbina00 <edurbina2002@hotmail.com>
2023-05-13 21:55:04 -0600

    Sin cambios nuevos

--- mainwindow.py
+++ mainwindow.py
@@ -2,12 +2,25 @@ from ui_mainwindow import *
 from PySide2 import QtCore, QtWidgets
 from PySide2.QtCore import QPropertyAnimation
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        # Lista  de instrucciones reconocidas
+        self.tabop = ("AAA", "AAD", "AAM", "AAS", "ADC", "ADD", "AND", "CALL", "CBW", "CLC",
+         "CLD", "CLI", "CMC", "CMP", "CMPSB", "CMPSW", "CWD", "DAA", "DAS",
+         "DEC", "DIV", "HLT", "IDIV", "IMUL", "IN", "INC", "INT", "INTO", "IRET",
+         "JA", "JAE", "JB", "JBE", "JC", "JCXZ", "JE", "JG", "JGE", "JL", "JLE", "JMP",
+         "JNA", "JNAE", "JNB", "JNBE", "JNC", "JNE", "JNG", "JNGE", "JNL", "JNLE",
+         "JNO", "JNP", "JNS", "JNZ", "JO", "JP", "JPE", "JPO", "JS", "JZ", "LAHF",
+         "LDS", "LEA", "LES", "LODSB", "LODSW", "LOOP", "LOOPE", "LOOPNE", "LOOPNZ",
+         "LOOPZ", "MOV", "MOVSB", "MOVSW", "MUL", "NEG", "NOP", "NOT", "OR", "OUT",
+         "POP", "POPA", "POPF", "PUSH", "PUSHF", "RCL", "RCR", "REP", "REPE", "REPNE",
+         "REPNZ", "REPZ", "RET", "RETF", "ROL", "ROR", "SAHF", "SAL", "SAR", "SBB", "SCASB",
+         "SCASW", "SHL", "SHR", "STC", "STD", "STI" "TOSB", "STOSW", "SUB", "TEST", "XCHG",
+         "XLATB", "XOR", ".ORG", "ORG")
         
         # Eventos ----------------------------------------------------------------
         
@@ -41,26 +54,25 @@ class MainWindow(QMainWindow):
         # Botones
         # -------------------------------------------------------------------------------
         self.ui.btn_correr.clicked.connect(self.error)
-        self.ui.btn_correr.clicked.connect(self.error)
         
         # Ajustar Tablas
         # self.ui.tabla_viajes.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) 
         
     # Funciones interfaz
     # -----------------------------------------------------------------------------------
-    def salir(self):
-        self.close()
-    def minimizar(self):
+    def salir(self):                                    # Funcion para cerrar ventana
+        self.close()                    
+    def minimizar(self):                                # Funcion para minimizar ventana
         self.showMinimized()
-    def normal(self):
+    def normal(self):                                   # Funcion para restaurar ventana
         self.showNormal()
         self.ui.btn_restaurar.hide()
         self.ui.btn_maximizar.show()
-    def maximizar(self):
+    def maximizar(self):                                # Funcion para maximizar ventana
         self.showMaximized()
         self.ui.btn_restaurar.show()
         self.ui.btn_maximizar.hide()
-    def mostrar_menu(self):
+    def mostrar_menu(self):                             # Funcion para invertir mostrar/ocultar menu
         if True:
             width = self.ui.frame_menu.width()
             normal = 0
@@ -75,24 +87,75 @@ class MainWindow(QMainWindow):
             self.animacion.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
             self.animacion.start()
     # SizeGrip
-    def resizeEvent(self, event):
+    def resizeEvent(self, event):                       # Funcion para modificar el tamaño de la ventana
         rect = self.rect()
         self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
     # Mover ventana
-    def mousePressEvent(self, event):
+    def mousePressEvent(self, event):                   # Detectar click del mouse
         self.clickPosition = event.globalPos()
-    def mover_ventana(self, event):
+    def mover_ventana(self, event):                     # Funcion para mover la ventana
         if not self.isMaximized():
             if event.buttons() == Qt.LeftButton:
                 self.move(self.pos() + event.globalPos() - self.clickPosition)
                 self.clickPosition = event.globalPos()
                 event.accept()
-        
         if event.globalPos().y() <= 20:
             self.showMaximized()
         else:
             self.showNormal()
             
-    def error(self):
-        n = [1, 3, 4, 7]
-        self.ui.plainTextEdit_codigo.highlight_error_lines(n)
+    def error(self):                                    # Funcion para resaltar lineas de codigo en rojo
+        self.ui.plainTextEdit_codigo.highlight_error_lines(self.procesar_codigo(self.read_lines()))
+        
+    def read_lines(self):                               # Lee todas las lineas del plaintext de codigo
+        contenido = self.ui.plainTextEdit_codigo.toPlainText()
+        lista_lineas = contenido.split('\n')
+        return lista_lineas                             # Regresa una lista de lineas
+    
+    def procesar_codigo(self, lista_lineas):            # Procesador de texto (descompone cada linea en componentes como: Instruccion, operadores, comentarios)
+        instruccion = ""
+        operandos = [""]
+        lista_erroes = []
+        contador_linea = 0
+        bandera = False
+        for linea in lista_lineas:
+            contador_linea += 1
+            if linea:
+                try:
+                    componentes = linea.split(';')      # Separar la línea por '; '
+                    instruccion_ope = componentes[0].split()      # Separar la primera parte de componentes por espacios
+                    instruccion = instruccion_ope[0]              # Obtener la instruccion de la linea
+                    try:
+                        operandos = instruccion_ope[1:] if len(instruccion_ope) > 1 else []
+                        operandos = operandos[0].split(",")       # Obtener los operandos de la instruccion
+                        comentario = componentes[1].strip() if len(componentes) > 1 else ""
+                    except:
+                        operandos = [""]
+                        comentario = ""
+                except:
+                    instruccion = ""
+            if instruccion.upper() in self.tabop:
+                if (instruccion.upper() != '.ORG') and (instruccion.upper() != 'ORG'):
+                    if linea != "":
+                        print('La linea "{}" es una linea valida!'.format(linea))
+                        print('La instruccion es: "{}", el o los operadores son: "{}", y el comentario es: "{}"'.format(instruccion, operandos, comentario))
+                        bandera = True
+            elif operandos[0].upper() in self.tabop:
+                try:
+                    if instruccion_ope[1].upper() in self.tabop:
+                        instruccion = instruccion_ope[1]
+                        operandos = instruccion_ope[2:] if len(instruccion_ope) > 1 else []
+                        operandos = operandos[0].split(",")
+                        comentario = componentes[1].strip() if len(componentes) > 1 else ""
+                        print('La linea "{}" es una linea valida!'.format(linea))
+                        print('La instruccion es: "{}", el o los operadores son: "{}", y el comentario es: "{}"'.format(instruccion, operandos, comentario))
+                except:
+                    pass
+            elif bandera:
+                if linea:
+                    lista_erroes.append(contador_linea)
+                    print(linea, " Sin instruccion")
+        return lista_erroes        
+        
+    
+    
