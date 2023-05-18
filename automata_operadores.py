708ea6eb2f40c00a03e3f36a1f593dde5b9114b6
Cesar Lomeli <cesarlomeli01@gmail.com>
2023-05-17 18:05:18 -0600

    Automata de operadores y de vars

--- automata_operadores.py
+++ automata_operadores.py
@@ -19,7 +19,6 @@ class Automata():
                 estado_actual = self.transiciones[(estado_actual, simbolo)]
             else:
                 return False
-
         return estado_actual in self.estados_aceptacion
     def acepta_cadena(self, cadena):
         estado_actual = self.estado_inicial
@@ -40,25 +39,85 @@ class Automata():
         return estado_actual in self.estados_aceptacion
 
 # Ejemplo de uso
-estados = {'q0', 'q1', 'q2'}
+estados = {'q0', 'q1', 'q2', 'q3'}
 estado_inicial = 'q0'
-estados_aceptacion = {'q2'}
+estados_aceptacion = {'q1','q2','q3','q4'}
 transiciones = {
-    (r'[0-9]', 'q1'),
-    (r'[a-z]', 'q0'),
-    (r'[A-Z]', 'q0')
+    ('q0', '0'): 'q1',
+    ('q0', '1'): 'q4',
+    ('q0', '2'): 'q4',
+    ('q0', '3'): 'q4',
+    ('q0', '4'): 'q4',
+    ('q0', '5'): 'q4',
+    ('q0', '6'): 'q4',
+    ('q0', '7'): 'q4',
+    ('q0', '8'): 'q4',
+    ('q0', '9'): 'q4',
+
+    ('q1', 'x'): 'q2',
+    ('q1', '0'): 'q4',
+    ('q1', '1'): 'q4',
+    ('q1', '2'): 'q4',
+    ('q1', '3'): 'q4',
+    ('q1', '4'): 'q4',
+    ('q1', '5'): 'q4',
+    ('q1', '6'): 'q4',
+    ('q1', '7'): 'q4',
+    ('q1', '8'): 'q4',
+    ('q1', '9'): 'q4',
+
+    ('q2', '0'): 'q2',
+    ('q2', '1'): 'q2',
+    ('q2', '2'): 'q2',
+    ('q2', '3'): 'q2',
+    ('q2', '4'): 'q2',
+    ('q2', '5'): 'q2',
+    ('q2', '6'): 'q2',
+    ('q2', '7'): 'q2',
+    ('q2', '8'): 'q2',
+    ('q2', '9'): 'q2',
+    ('q2', 'a'): 'q2',
+    ('q2', 'b'): 'q2',
+    ('q2', 'c'): 'q2',
+    ('q2', 'd'): 'q2',
+    ('q2', 'e'): 'q2',
+    ('q2', 'f'): 'q2',
+    ('q2', 'h'): 'q3',
+
+    ('q4', '0'): 'q4',
+    ('q4', '1'): 'q4',
+    ('q4', '2'): 'q4',
+    ('q4', '3'): 'q4',
+    ('q4', '4'): 'q4',
+    ('q4', '5'): 'q4',
+    ('q4', '6'): 'q4',
+    ('q4', '7'): 'q4',
+    ('q4', '8'): 'q4',
+    ('q4', '9'): 'q4',
+    ('q4', 'a'): 'q4',
+    ('q4', 'b'): 'q4',
+    ('q4', 'c'): 'q4',
+    ('q4', 'd'): 'q4',
+    ('q4', 'e'): 'q4',
+    ('q4', 'f'): 'q4',
+    ('q4', 'h'): 'q3'
 }
 
-# automata = Automata(estados, estado_inicial, estados_aceptacion, transiciones)
+automata = Automata(estados, estado_inicial, estados_aceptacion, transiciones)
 
-cadena1 = 'aB3'
-cadena2 = '123'
-cadena3 = 'AbC'
+cadena1 = '0x100h'
+cadena2 = '0'
+cadena3 = '010'
+cadena4 = '10'
+cadena5 = '0x1f'
+cadena6 = 'af'
 
-# print(automata.acepta_cadena(cadena1))  # True
-# print(automata.acepta_cadena(cadena2))  # False
-# print(automata.acepta_cadena(cadena3))  # False
-
+print(automata.acepta_cadena_caracter(cadena1))  # True
+print(automata.acepta_cadena_caracter(cadena2))  # False
+print(automata.acepta_cadena_caracter(cadena3))  # False
+print(automata.acepta_cadena_caracter(cadena4))
+print(automata.acepta_cadena_caracter(cadena5))
+print(automata.acepta_cadena_caracter(cadena6))
 """
 # Ejemplo de uso
 estados = {'q0', 'q1', 'q2'}
